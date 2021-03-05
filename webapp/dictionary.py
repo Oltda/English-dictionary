94c7fda824f836340f0199e6fef6713a77f826fe
Daniel Olt <dan.olt@hotmail.cz>
2021-03-05 15:46:52 +0700

    final instant result

--- webapp/dictionary.py
+++ webapp/dictionary.py
@@ -12,22 +12,22 @@ class Dictionary:
         jp.Div(a=div, text="Get the definition of any English word as you type!")
 
         input_div = jp.Div(a=div, classes="grid grid-cols-2")
-        input_box = jp.Input(a=input_div, placeholder="Type a word here...",
-                 classes="m-2 bg-gray-100 border-2 border-gray-200 rounded w-64 "
-                         "focus:bg-white focus:outline-none focus:border-purple-500 py-2 px-4 ")
-
 
         output_div = jp.Div(a=div, classes="m-2 p-2 text-lg border-2 h-40")
 
-        jp.Button(a=input_div, text="Get definition", classes="border-2 text-gray-500",
-                  click=cls.get_definition, inputbox=input_box, outputdiv=output_div)
+        input_box = jp.Input(a=input_div, placeholder="Type a word here...", outputdiv=output_div,
+                 classes="m-2 bg-gray-100 border-2 border-gray-200 rounded w-64 "
+                         "focus:bg-white focus:outline-none focus:border-purple-500 py-2 px-4 ")
+
+        input_box.on('input', cls.get_definition)
+
 
 
         return wp
 
     @staticmethod
     def get_definition(widget, msg):
-        defined = definition.Definition(widget.inputbox.value).get()
+        defined = definition.Definition(widget.value).get()
         widget.outputdiv.text = " ".join(defined)
 
 
