c6c4e4706707f3a8885e2bb14573d29cb0ad1f03
Daniel Olt <dan.olt@hotmail.cz>
2021-03-05 15:36:03 +0700

    final with button that gets the definition

--- definition.py
+++ definition.py
@@ -12,9 +12,8 @@ class Definition:
         df = pandas.read_csv("data.csv")
         return tuple(df.loc[df['word'] == self.term]['definition'])
 
-d = Definition(term="sun")
 
-print(d.get())
+
 
 
 
